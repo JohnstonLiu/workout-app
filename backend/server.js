a64f0eb17082dccfa15cc6f4bef71d4ce1009e64
Johnston Liu <johnstonliu2004@gmail.com>
2024-01-02 19:05:00 -0600

    setup updates

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,22 @@
 require('dotenv').config();
 
 const express = require('express');
-
+const workoutRoutes = require('./routes/workouts');
 const app = express();
+
+// middleware
+app.use(express.json);
 
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 });
 
-app.get('/', (req, res) => {
-    res.json({msg: 'Welcome to the app'});
-});
+// routes
+app.use('/api/workouts', workoutRoutes);
 
-app.listen(process.env.PORT, () => {
-    console.log('listening on port 4000');
-});
+
+app.listen(
+    process.env.PORT, 
+    () => console.log('listening on port 4000')
+);
